e8372651571b422df9e84c0ce1c2f8e56a962ba0
1 <a1@MacBook-Pro-1.tonicforhealth.com>
2016-07-14 13:18:18 +0300

    Initial commit

---
+++ src/main/java/setup/SeleniumDriver.java
@@ -0,0 +1,20 @@
+package main.java.setup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by a1 on 14.07.16.
+ */
+public class SeleniumDriver {
+
+    static WebDriver driver;
+    public static WebDriver getDriver() {
+//        if (driver==null) {
+//            DesiredCapabilities
+//
+//        }
+        return null;
+    }
+
+}
